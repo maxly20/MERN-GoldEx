7c2eaeda1d9831594f791d4efa24b5533bf5bb0a
maxly20 <maxly2011@gmail.com>
2021-05-11 13:20:42 +1000

    building navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,10 @@
+import Navbar from './components/Navbar';
+// import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 const App = () => {
   return (
     <div className='App'>
-      <h1>HELLLOOOO</h1>
+      <Navbar />
     </div>
   );
 };
