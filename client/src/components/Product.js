b0a7a9ae86f8a2f8f3931b7bf774c4850ff0e104
maxly20 <maxly2011@gmail.com>
2021-05-11 17:03:59 +1000

    finish productscreen

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,13 +1,19 @@
 import { Link } from 'react-router-dom';
+import Rating from 'react-rating';
 
 const Product = ({ product }) => {
   return (
-    <div className='col-md-3 m-5 card p-2'>
+    <div className='col-md-3 m-3 card p-2 text-left'>
       <div>
         <Link to={`product/${product.id}`}>
           <img src={product.image} className='img-fluid' alt={product.name} />
           <h1>{product.name}</h1>
-          <h1>Rating: {product.rating}</h1>
+          <Rating
+            style={{ color: 'orange' }}
+            initialRating={product.rating}
+            emptySymbol='fa fa-star-o fa-1x'
+            fullSymbol='fa fa-star fa-1x'
+          />
           <h1>Price: {product.price}</h1>
         </Link>
       </div>
