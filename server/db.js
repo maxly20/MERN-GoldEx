ac2d7703448fe4b437ba8891a85b2a7870f01f41
maxly20 <maxly2011@gmail.com>
2021-05-15 21:25:03 +1000

    set up mongodb

--- server/db.js
+++ server/db.js
@@ -1,2 +1,5 @@
 const { Mongoose } = require("mongoose");
 const mongoose = require("mongoose");
+
+
+const mongoDBURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}?retryWrites=true&w=majority`
