6a07f09116d67ed737c03f02a688a7a6fb447618
maxly20 <maxly2011@gmail.com>
2021-05-20 11:34:50 +1000

    adding mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 require('dotenv').config();
 const express = require('express');
+const connectDB = require('./config/db');
+
+connectDB();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
