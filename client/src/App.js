eb4f0bf19155ee46addf27ff639546c393de2eff
maxly20 <maxly2011@gmail.com>
2021-05-20 11:07:59 +1000

    reset project]

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,7 @@
-import { Route, Switch } from 'react-router-dom';
-import Navbar from './components/Navbar';
-import Homescreen from './screens/Homescreen';
-import Productdescscreen from './screens/Productdescscreen';
-// import bootstrap from '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import { Route, Switch } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div className='App'>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={Homescreen} />
-        <Route path='/product/:id' component={Productdescscreen} />
-      </Switch>
-    </div>
-  );
+  return <div className='App'>HELLOOO</div>;
 };
 
 export default App;
