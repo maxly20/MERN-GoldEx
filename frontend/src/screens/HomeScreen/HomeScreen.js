54f08812cb8be3985922fad15b8a0d814718ba3a
maxly20 <maxly2011@gmail.com>
2021-05-28 21:07:50 +1000

    adding products

--- frontend/src/screens/HomeScreen/HomeScreen.js
+++ frontend/src/screens/HomeScreen/HomeScreen.js
@@ -7,12 +7,7 @@ const HomeScreen = () => {
       <h2 className='homescreen__title'>Latest Products</h2>
       <div className='homescreen__products'>
         <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
-        <Product />
+
       </div>
     </div>
   );
---
+++ newproduct.js
@@ -0,0 +1,16 @@
+// https://i.ibb.co/kgq1pqd/Sweetiee-14.jpg
+// https://i.ibb.co/8syVb9r/Sweetiee-16.jpg
+// https://i.ibb.co/5kzPPdP/Sweetiee-15.jpg
+// https://i.ibb.co/GJwwGdY/Sweetiee-13.jpg
+// https://i.ibb.co/hKypV5z/Sweetiee-12.jpg
+// https://i.ibb.co/pvx7nn0/Sweetiee-11.jpg
+// https://i.ibb.co/fXJ0VCh/Sweetiee-10.jpg
+// https://i.ibb.co/dmQXybS/Sweetiee-9.jpg
+// https://i.ibb.co/njCy9JH/Sweetiee-8.jpg
+// https://i.ibb.co/S5FCMNg/Sweetiee-7.jpg
+// https://i.ibb.co/VMnv3b5/Sweetiee-6.jpg
+// https://i.ibb.co/xHsF2wS/Sweetiee-5.jpg
+// https://i.ibb.co/TkzdFJ9/Sweetiee-4.jpg
+// https://i.ibb.co/YD7zcx7/Sweetiee-3.jpg
+// https://i.ibb.co/9823g7Q/Sweetiee-2.jpg
+// https://i.ibb.co/cD97jPm/Sweetiee-1.jpg
